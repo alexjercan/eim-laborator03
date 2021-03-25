9700008e31fb326984af55d9c321124ad6f4ea28
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-03-25 09:56:34 +0200

    a

---
+++ PhoneDialer/app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/general/Constants.java
@@ -0,0 +1,22 @@
+package ro.pub.cs.systems.eim.lab03.phonedialer.general;
+
+import ro.pub.cs.systems.eim.lab03.phonedialer.R;
+
+public interface Constants {
+
+    final public static int buttonIds[] = {
+            R.id.button_0,
+            R.id.button_1,
+            R.id.button_2,
+            R.id.button_3,
+            R.id.button_4,
+            R.id.button_5,
+            R.id.button_6,
+            R.id.button_7,
+            R.id.button_8,
+            R.id.button_9,
+            R.id.button_star,
+            R.id.button_hash
+    };
+    final public static int PERMISSION_REQUEST_CALL_PHONE = 1;
+}
